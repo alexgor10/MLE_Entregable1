4d2bc7de48da9b37bbbb397f51cb867a51eb89ba
Paucar <BAUSTRO\ba01006607@mwba01006607.austro.grpfin>
2024-02-02 15:48:12 -0500

    Commit inicial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "MLEngineer_Practica1",
+    version = "0.1",
+    license = "MIT",
+    description = "Paquete de proyecto Practica 1",
+    author= "Alexander Paucar y Stephen Morales",
+    author_email = "alexgor93@outlook.com"
+)
